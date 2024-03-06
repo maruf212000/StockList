4de63c8a7a25aea1f957f4847420dfc3278f3f62
maruf212000 <maruf.memon21@gmail.com>
2024-03-06 19:00:52 +0530

    Initial Commit

---
+++ StockList/StockListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockListApp.swift
+//  StockList
+//
+//  Created by Maruf Memon on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct StockListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
